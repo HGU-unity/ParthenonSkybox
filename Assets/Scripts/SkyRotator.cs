cd07b68df065d086b3d83d087a889f100e153286
Geoyeob Kim <gasbank@gmail.com>
2018-01-26 23:45:08 +0900

    처음

---
+++ Assets/Scripts/SkyRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SkyRotator : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.LogFormat("{0}: SkyRotator Start()", gameObject.name);
+    }
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time);
+    }
+}
---
+++ Assets/Scripts/StartOrder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class StartOrder : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.LogFormat("{0}: Start()", gameObject.name);
+    }
+}
